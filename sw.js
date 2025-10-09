123d85bad609ea9f160f28c2b9bbbf1d1e3db1b8
Gasket-Spencer <90284811+Gasket-Spencer@users.noreply.github.com>
2025-10-09 12:21:00 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+self.addEventListener('push', event => {
+    console.log('Push received:', event);
+
+    let data = {};
+    if (event.data) {
+        data = event.data.json();
+    }
+
+    const title = data.title || "New Notification";
+    const options = {
+        body: data.body || "Test notification",
+    };
+
+    event.waitUntil(
+        self.registration.showNotification(title, options)
+    );
+});
+
+self.addEventListener('notificationclick', event => {
+    event.notification.close();
+    event.waitUntil(
+        // Opens site
+        clients.openWindow('/')
+    );
+});
